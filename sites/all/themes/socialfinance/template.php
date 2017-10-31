eff940313cda55cea62c293a86c045a79cf105dd
root <root@arguweb06.tabd.co.uk>
2017-10-31 17:50:12 +0000

    Prelaunch changes

--- sites/all/themes/socialfinance/template.php
+++ sites/all/themes/socialfinance/template.php
@@ -5,3 +5,10 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * socialfinance theme.
  */
+
+
+/* RESETS USERNAME LENGTH - FOR GERALDINE PEACOCK */
+function socialfinance_preprocess_username(&$vars) {
+	//putting back what drupal core messed with
+	$vars['name'] = check_plain($vars['name_raw']);
+}
